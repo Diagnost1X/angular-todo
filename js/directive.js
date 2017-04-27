32e49677480956eee8d43a82661b1df28fe616cf
Ashley Shenton <Shent@Ashleys-Mac.local>
2017-04-27 13:40:19 +0100

    changed navbar to a directive

--- js/directive.js
+++ js/directive.js
@@ -1,6 +1,14 @@
-angular.module("TodoDirective", []).directive("todoTable", function() {
-	return {
-		restrict: "E", //A -> attribute E -> element
-		templateUrl: "templates/directives/todo-table.html"
-	};
-});
+angular.module("TodoDirective", [])
+	.directive("todoTable", function() {
+		return {
+			restrict: "E", //A -> attribute E -> element
+			templateUrl: "templates/directives/todo-table.html"
+		};
+	});
+angular.module("NavbarDirective", [])
+	.directive("navbar", function() {
+		return {
+			restrict: "E",
+			templateUrl: "templates/directives/navbar.html"
+		};
+	});
